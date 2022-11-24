221e46bb0f6ca397693962a88c9533492244acf4
foysal1021 <foysala1021@gmail.com>
2022-11-24 17:02:54 +0600

    added a banner

---
+++ src/Conmopent/Home/Banner/Banner.js
@@ -0,0 +1,18 @@
+import React from "react";
+import bannerImg from "../../../Assat/Img/smartphone.14bb354.png";
+
+const Banner = () => {
+  return (
+    <div className=" flex justify-between items-center">
+      <div>
+        {" "}
+        SELL YOUR <br></br> <span> SMARTPHONE</span> <br></br> FOR QUICK CASH
+      </div>
+      <div>
+        <img src={bannerImg} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
